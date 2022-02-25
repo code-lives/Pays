43825cd9d6c18584179384409ae7dd488b4dcf2f
LiJie <79346591+code-lives@users.noreply.github.com>
2022-02-25 13:34:32 +0800

    Update Byte.php

--- src/Byte.php
+++ src/Byte.php
@@ -2,36 +2,62 @@
 
 namespace Applet\Pay;
 
-class Kuaishou implements PayInterface
+class Byte implements PayInterface
 {
 
     private $orderParam;
     private $app_id;
-    private $app_secret;
+    private $secret;
+    private $merchant_id;
+    private $salt;
+    private $valid_time;
     private $notify_url;
+    private $settle_url;
     private $token;
-    private $codedUrl = 'https://open.kuaishou.com/oauth2/mp/code2session';
-    private $tokenUrl = 'https://open.kuaishou.com/oauth2/access_token';
-    protected $payUrl = 'https://open.kuaishou.com/openapi/mp/developer/epay/create_order';
-    protected $query = 'https://open.kuaishou.com/openapi/mp/developer/epay/query_order';
-    protected $refundUrl = 'https://open.kuaishou.com/openapi/mp/developer/epay/apply_refund';
-    protected $notifyOrder;
+    private $codedUrl = 'https://minigame.zijieapi.com/mgplatform/api/apps/jscode2session?';
+    private $tokenUrl = 'https://minigame.zijieapi.com/mgplatform/api/apps/token';
+    protected $payUrl = 'https://developer.toutiao.com/api/apps/ecpay/v1/create_order';
+    protected $query = 'https://developer.toutiao.com/api/apps/ecpay/v1/query_order';
+    protected $refundUrl = 'https://developer.toutiao.com/api/apps/ecpay/v1/create_refund';
+    protected $settle = 'https://developer.toutiao.com/api/apps/ecpay/v1/settle';
+    protected $payOrder;
+    private $notifyOrder;
 
     public static function init($config)
     {
         if (!isset($config['app_id']) || empty($config['app_id'])) {
             throw new \Exception('not empty app_id');
         }
-        if (!isset($config['app_secret']) || empty($config['app_secret'])) {
+
+        if (!isset($config['secret']) || empty($config['secret'])) {
             throw new \Exception('not empty secret');
         }
+
+        if (!isset($config['merchant_id']) || empty($config['merchant_id'])) {
+            throw new \Exception('not empty merchant_id');
+        }
+
+        if (!isset($config['salt']) || empty($config['salt'])) {
+            throw new \Exception('not empty salt');
+        }
+
         if (!isset($config['notify_url']) || empty($config['notify_url'])) {
             throw new \Exception('not empty notify_url');
         }
+
+        if (!isset($config['token']) || empty($config['token'])) {
+            throw new \Exception('not empty notify_url');
+        }
+
         $class = new self();
         $class->app_id = $config['app_id'];
-        $class->app_secret = $config['app_secret'];
+        $class->secret = $config['secret'];
+        $class->token = $config['token'];
+        $class->merchant_id = $config['merchant_id'];
+        $class->salt = $config['salt'];
+        $class->settle_url = isset($config['settle_url']) ? $config['settle_url'] : $config['notify_url'];
         $class->notify_url = $config['notify_url'];
+        $class->valid_time = isset($config['valid_time']) ? $config['valid_time'] : time() + 900;
         return $class;
     }
     /**
@@ -39,43 +65,40 @@ class Kuaishou implements PayInterface
      */
     public function getParam()
     {
-        return $this->orderParam;
+        return $this->payOrder;
     }
     /**
      * 获取异步订单信息
      */
     public function getNotifyOrder()
     {
-        $bodyData = file_get_contents("php://input");
-        $this->notifyOrder = json_decode($bodyData, true);
+        $data = file_get_contents("php://input");
+        $order = json_decode($data, true);
+        $order['msg'] = json_decode($order['msg'], true);
+        $this->notifyOrder = $order;
         return $this->notifyOrder;
     }
     /**
      * 设置订单号 金额 描述
-     * @param string $order_no 平台订单号
+     * @param string $rder_no 平台订单号
      * @param int $money 订单金额
      * @param string $title 描述
-     * @param string $desc 详细
-     * @param string $openid 用户openid
-     * @param string @access_token 获取access_token
+     *
      */
-    public function set($order_no, $money, $title, $desc, $openid, $access_token)
+    public function set($order_no, $money, $title, $desc = '')
     {
-        $order = [
-            'app_id' => $this->app_id,
-            'out_order_no' => $order_no,
-            'open_id' => $openid,
-            'total_amount' => $money * 100,
-            'subject' => $title,
-            'detail' => $desc,
-            'type' => 3306,
-            'expire_time' => 3000,
-            'attach' => '11',
-            'notify_url' => $this->notify_url,
-        ];
-        $order['sign'] = $this->sign($order);
-        $url = $this->payUrl . "?app_id=" . $this->app_id . "&access_token=" . $access_token;
-        $this->orderParam = $this->curl_post_json($url, json_encode($order));
+        $this->orderParam['out_order_no'] = $order_no;
+        $this->orderParam['total_amount'] = $money * 100;
+        $this->orderParam['subject'] = $title;
+        $this->orderParam['body'] = $desc;
+        $this->orderParam['notify_url'] = $this->notify_url;
+        $this->orderParam['valid_time'] = $this->valid_time;
+        $this->orderParam['app_id'] = $this->app_id;
+        $data = json_encode(['sign' => $this->sign($this->orderParam)] + $this->orderParam);
+        $result = json_decode($this->curl_post($this->payUrl, $data), true);
+        if (isset($result['err_no']) && $result['err_no'] == 0) {
+            $this->payOrder = $result['data'];
+        }
         return $this;
     }
     /**
@@ -84,209 +107,139 @@ class Kuaishou implements PayInterface
      */
     public function sign(array $map)
     {
+        $rList = array();
         foreach ($map as $k => $v) {
-            $Parameters[$k] = $v;
-        }
-        //签名步骤一：按字典序排序参数
-        ksort($Parameters);
-        $String = $this->formatBizQueryParaMap($Parameters, false);
-        //签名步骤二：在string后加入KEY
-        $String = $String . $this->app_secret;
-        //签名步骤三：MD5加密
-        $String = md5($String);
-        // //签名步骤四：所有字符转为大写
-        return $String;
-    }
-    /**
-     *    作用：格式化参数，签名过程需要使用
-     */
-    public function formatBizQueryParaMap($paraMap, $urlencode)
-    {
-        $buff = "";
-        ksort($paraMap);
-        foreach ($paraMap as $k => $v) {
-            if ($urlencode) {
-                $v = urlencode($v);
+            if ($k == "other_settle_params" || $k == "app_id" || $k == "sign" || $k == "thirdparty_id") {
+                continue;
             }
-            $buff .= $k . "=" . $v . "&";
+
+            $value = trim(strval($v));
+            $len = strlen($value);
+            if ($len > 1 && substr($value, 0, 1) == "\"" && substr($value, $len, $len - 1) == "\"") {
+                $value = substr($value, 1, $len - 1);
+            }
+
+            $value = trim($value);
+            if ($value == "" || $value == "null") {
+                continue;
+            }
+
+            array_push($rList, $value);
         }
-        $reqPar = '';
-        if (strlen($buff) > 0) {
-            $reqPar = substr($buff, 0, strlen($buff) - 1);
-        }
-        return $reqPar;
+        array_push($rList, $this->salt);
+        sort($rList, 2);
+        return md5(implode('&', $rList));
     }
     /**
      * 获取token
-     * @return array
      */
     public function getToken()
     {
         $arr = [
-            'grant_type' => 'client_credentials',
-            'app_id' => $this->app_id,
-            'app_secret' => $this->app_secret,
+            'grant_type' => 'client_credential',
+            'appid' => $this->app_id,
+            'secret' => $this->secret,
         ];
-        $result = json_decode($this->curl_post($this->tokenUrl, $arr), true);
-        if ($result['result'] != 1) {
-            throw new \Exception($result['error_msg']);
+        $result = json_decode($this->curl_post($this->tokenUrl, json_encode($arr)), true);
+        if (isset($result['err_no']) && $result['err_tips'] == "success") {
+            return $result['data'];
         }
-        return $result;
+        throw new \Exception($result['err_no']);
     }
     /**
-     * 获取openid
-     * @param string $code
-     * @return array 成功返回数组 失败为空
+     * 获取 openid
+     *
+     * @param  string $code
+     * @param  string $anonymous_code
+     * @return void
+     * @author LiJie
      */
-    public function getOpenid($code)
+    public function getOpenid($code, $anonymous_code)
     {
-        $data = [
-            'js_code' => $code,
-            'app_id' => $this->app_id,
-            'app_secret' => $this->app_secret,
-        ];
-        $result = json_decode($this->curl_post($this->codedUrl, $data), true);
-        if ($result['result'] != 1) {
-            throw new \Exception('invalid js_code');
+        $url = $this->codedUrl . "appid=" . $this->app_id . "&secret=" . $this->secret . "&code=" . $code . "&anonymous_code=" . $anonymous_code;
+        $result = json_decode($this->curl_get($url), true);
+        if ($result['error'] == 0) {
+            return $result;
         }
-        $result['openid'] = $result['open_id'];
-        return $result;
+        throw new \Exception($result['err_no']);
     }
     /**
      * 异步回调
-     * @param string $order 回调数据
-     * @param string $kwaisign sign
+     * @param  $order 回调数据
      * @return bool true   验签通过|false 验签不通过
      */
     public function notifyCheck()
     {
-        if (md5(json_encode($this->notifyOrder) . $this->app_secret) != $_SERVER['HTTP_KWAISIGN']) {
-            return false;
+        $order = $this->getNotifyOrder();
+        $data = [
+            $order['timestamp'],
+            $order['nonce'],
+            json_encode($order['msg']),
+            $this->token,
+        ];
+
+        sort($data, SORT_STRING);
+        $str = implode('', $data);
+        if (!strcmp(sha1($str), $order['msg_signature'])) {
+            return true;
         }
-        return true;
+        return false;
     }
     /**
      * 申请退款
-     * @param string $order['out_order_no'] 订单号
-     * @param string $order['out_refund_no'] 自定义退款单号
-     * @param string $order['reason'] 推荐原因
-     * @param string $order['attach'] 自定义字段
-     * @param string $order['refund_amount'] 金额
-     * @param string $order['access_token'] access_token
+     *
      */
     public function applyOrderRefund($order)
     {
-        $orders = [
-            'app_id' => $this->app_id,
-            'out_order_no' => $order['out_order_no'],
-            'out_refund_no' => $order['out_refund_no'],
-            'reason' => $order['reason'],
-            'notify_url' => $this->notify_url,
-            'attach' => $order['attach'],
-            'refund_amount' => $order['refund_amount'] * 100,
-        ];
-        $orders['sign'] = $this->sign($orders);
-        $url = $this->refundUrl . "?app_id=" . $this->app_id . "&access_token=" . $order['access_token'];
-        return json_decode($this->curl_post_json($url, json_encode($orders)), true);
+        $order['notify_url'] = $this->notify_url;
+        $order['app_id'] = $this->app_id;
+        $order['refund_amount'] *= 100;
+        return json_decode($this->curl_post($this->refundUrl, json_encode(['sign' => $this->sign($order)] + $order)), true);
+
     }
     /**
-     * 查询订单
-     * @param string $out_order_no
-     * @param string $access_token
-     * @param string $bodyParam
-     * @param string $url
-     * @param void
+     * 订单查询
+     * @param string $out_order_no 订单号
+     * @return array 订单信息
      */
-    public function findOrder($out_order_no, $access_token)
+    public function findOrder($out_order_no)
     {
+        if (empty($out_order_no)) {
+            return false;
+        }
         $order = [
-            'app_id' => $this->app_id,
             'out_order_no' => $out_order_no,
+            'app_id' => $this->app_id,
         ];
-        $bodyParam['sign'] = $this->sign($order);
-        $bodyParam['out_order_no'] = $out_order_no;
-        $url = $this->query . "?app_id=" . $this->app_id . "&access_token=" . $access_token;
-        return json_decode($this->curl_post_json($url, json_encode($bodyParam)), true);
-
+        $order['sign'] = $this->sign($order);
+        $result = json_decode($this->curl_post($this->query, json_encode($order)), true);
+        return $result;
     }
     /**
-     * curl post json传递
+     * 分账
      *
-     * @param string $url
-     * @param string $data json数据
-     * @return string $response
-     */
-    protected static function curl_post_json($url, $data)
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt(
-            $ch,
-            CURLOPT_HTTPHEADER,
-            array(
-                'Content-Type: application/json; charset=utf-8',
-                'Content-Length: ' . strlen($data),
-            )
-        );
-        $response = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        return $response;
-    }
-    /**
-     * curl get function
-     * @param string $url
+     * @param  [type] $order
      * @return void
+     * @author LiJie
      */
-    protected static function curl_get($url)
+    public function settle($order)
     {
-        $headerArr = array("Content-type:application/x-www-form-urlencoded");
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArr);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $output = curl_exec($curl);
-        curl_close($curl);
-        return $output;
-    }
-    /**
-     * 获取token 和 openid
-     *
-     * @param string $url
-     * @param array $data
-     * @return string $output
-     */
-    protected static function curl_post($url, $data)
-    {
-        $ch = curl_init();
+        $data = [
+            'app_id' => $this->app_id,
+            'out_settle_no' => $order['out_settle_no'],
+            'out_order_no' => $order['out_order_no'],
+            'settle_desc' => $order['settle_desc'],
+            'notify_url' => $this->settle_url,
+            'cp_extra' => $order['cp_extra'],
+        ];
+        $data['sign'] = $this->sign($data);
+        $result = json_decode($this->curl_post($this->query, json_encode($data)), true);
+        if (isset($result['err_no']) && $result['err_no'] == 0) {
+            return $result;
+        } else {
+            return false;
+        }
 
-        curl_setopt($ch, CURLOPT_URL, $url);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
-        // POST数据
-
-        curl_setopt($ch, CURLOPT_POST, 1);
-
-        // 把post的变量加上
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
-        $output = curl_exec($ch);
-
-        curl_close($ch);
-
-        return $output;
     }
     /**
      * 解密手机号
@@ -316,7 +269,59 @@ class Kuaishou implements PayInterface
         if ($dataObj == null) {
             return false;
         }
-
+        if ($dataObj->watermark->appid != $this->app_id) {
+            return false;
+        }
         return json_decode($result, true);
     }
+    protected static function curl_get($url)
+    {
+        $headerArr = array("Content-type:application/x-www-form-urlencoded");
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArr);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return $output;
+    }
+    /**
+     * @desc post 用于退款
+     */
+    protected static function curl_post($url, $data)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Content-Type: application/json;charset=utf-8',
+                'Content-Length: ' . strlen($data),
+            )
+        );
+        // POST数据
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+        // 把post的变量加上
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        $output = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $output;
+    }
+
 }
